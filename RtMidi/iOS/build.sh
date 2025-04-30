6d18c83a4d2a87b843d9bab1dfab6e4c4778b308
Keijiro Takahashi <keijiro@gmail.com>
2025-04-30 21:51:42 +0900

    use clang for iOS

--- RtMidi/iOS/build.sh
+++ RtMidi/iOS/build.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-
 SYSROOT=`xcrun --sdk iphoneos --show-sdk-path`
-FLAGS="-O2 -Wall --sysroot $SYSROOT -isysroot $SYSROOT -fembed-bitcode"
-FLAGS+=" -I../Source -D RTMIDI_EXPORT -D __MACOSX_CORE__"
-
-gcc $FLAGS -c ../Source/RtMidi.cpp
-gcc $FLAGS -c ../Source/rtmidi_c.cpp
-
+XFLAGS="-target arm64-apple-ios -isysroot $SYSROOT -fembed-bitcode"
+CCFLAGS="-O2 -Wall -Wextra -I../Source -D RTMIDI_EXPORT -D __MACOSX_CORE__"
+clang $XFLAGS $CCFLAGS -c ../Source/RtMidi.cpp
+clang $XFLAGS $CCFLAGS -c ../Source/rtmidi_c.cpp
 ar -crv libRtMidi.a RtMidi.o rtmidi_c.o
