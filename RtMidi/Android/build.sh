57b4ae5ae7073e2277285e569521861ae22f02e6
Keijiro Takahashi <keijiro@gmail.com>
2025-05-01 14:57:29 +0900

    enable amidi for Android

--- RtMidi/Android/build.sh
+++ RtMidi/Android/build.sh
@@ -3,9 +3,9 @@ ANDROID_NDK_PATH="/Applications/Unity/Hub/Editor/6000.0.47f1/PlaybackEngines/And
 BIN_PATH=$ANDROID_NDK_PATH/toolchains/llvm/prebuilt/darwin-x86_64/bin
 CC=$BIN_PATH/aarch64-linux-android30-clang++
 STRIP=$BIN_PATH/llvm-strip
-CCFLAGS="-O2 -Wall -I.. -fPIC"
+CCFLAGS="-O2 -Wall -fPIC -I.. -D RTMIDI_EXPORT -D __AMIDI__"
 LDFLAGS="-shared -rdynamic -fPIC -Wl,-z,max-page-size=16384"
-LIBS="-static-libstdc++"
+LIBS="-static-libstdc++ -lamidi"
 $CC $CCFLAGS -c ../Source/RtMidi.cpp
 $CC $CCFLAGS -c ../Source/rtmidi_c.cpp
 $CC $LDFLAGS RtMidi.o rtmidi_c.o $LIBS -o libRtMidi.so
