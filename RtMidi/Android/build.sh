53b4237edda4959b7c5d2b12d926cad7e7768c57
Keijiro Takahashi <keijiro@gmail.com>
2025-05-01 12:06:46 +0900

    Android binary

---
+++ RtMidi/Android/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ANDROID_NDK_PATH="/Applications/Unity/Hub/Editor/6000.0.47f1/PlaybackEngines/AndroidPlayer/NDK/"
+BIN_PATH=$ANDROID_NDK_PATH/toolchains/llvm/prebuilt/darwin-x86_64/bin
+CC=$BIN_PATH/aarch64-linux-android30-clang++
+STRIP=$BIN_PATH/llvm-strip
+CCFLAGS="-O2 -Wall -I.. -fPIC"
+LDFLAGS="-shared -rdynamic -fPIC -Wl,-z,max-page-size=16384"
+LIBS="-static-libstdc++"
+$CC $CCFLAGS -c ../Source/RtMidi.cpp
+$CC $CCFLAGS -c ../Source/rtmidi_c.cpp
+$CC $LDFLAGS RtMidi.o rtmidi_c.o $LIBS -o libRtMidi.so
+$STRIP libRtMidi.so
--- RtMidi/Windows/build.sh
+++ RtMidi/Windows/build.sh
@@ -3,7 +3,7 @@ CC="x86_64-w64-mingw32-g++"
 STRIP="x86_64-w64-mingw32-strip"
 CCFLAGS="-O2 -Wall -I../Source -D RTMIDI_EXPORT -D __WINDOWS_MM__"
 LDFLAGS="-shared"
-LIBS=" -Wl,-Bstatic -lwinmm -static-libgcc"
+LIBS="-Wl,-Bstatic -lwinmm -static-libgcc"
 $CC $CCFLAGS -c ../Source/RtMidi.cpp
 $CC $CCFLAGS -c ../Source/rtmidi_c.cpp
 $CC $LDFLAGS RtMidi.o rtmidi_c.o $LIBS -o RtMidi.dll
