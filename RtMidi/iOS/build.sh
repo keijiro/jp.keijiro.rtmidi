7d829f4c54c11ada0a3d0da00ca58fb63ae32b00
Keijiro Takahashi <keijiro@gmail.com>
2025-05-02 10:30:20 +0900

    simplification

--- RtMidi/iOS/build.sh
+++ RtMidi/iOS/build.sh
@@ -2,6 +2,5 @@
 SYSROOT=`xcrun --sdk iphoneos --show-sdk-path`
 XFLAGS="-target arm64-apple-ios -isysroot $SYSROOT -fembed-bitcode"
 CCFLAGS="-O2 -Wall -Wextra -I../Source -D RTMIDI_EXPORT -D __MACOSX_CORE__"
-clang $XFLAGS $CCFLAGS -c ../Source/RtMidi.cpp
-clang $XFLAGS $CCFLAGS -c ../Source/rtmidi_c.cpp
+clang $XFLAGS $CCFLAGS -c ../Source/RtMidi.cpp ../Source/rtmidi_c.cpp
 ar -crv libRtMidi.a RtMidi.o rtmidi_c.o
