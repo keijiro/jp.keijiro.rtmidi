ae1ded06e4deb6bddda2262ec75f333e056498f2
Keijiro Takahashi <keijiro@gmail.com>
2025-05-02 14:49:39 +0900

    Makefile

---
+++ RtMidi/Build/Common.mk
@@ -0,0 +1,76 @@
+#
+# File listings
+#
+
+PRODUCT = RtMidi
+
+SRCS = ../Source/RtMidi.cpp \
+       ../Source/rtmidi_c.cpp
+
+OBJ_DIR = $(PLATFORM)-$(ARCH)
+
+#
+# Intermediate/output files
+#
+
+OBJS = $(addprefix $(OBJ_DIR)/, $(notdir $(SRCS:.cpp=.o)))
+
+ifeq ($(TARGET_TYPE), dll)
+  TARGET = $(OBJ_DIR)/$(PRODUCT).$(TARGET_TYPE)
+else
+  TARGET = $(OBJ_DIR)/lib$(PRODUCT).$(TARGET_TYPE)
+endif
+
+#
+# Compiler/linker options
+#
+
+CCFLAGS += -O2 -Wall -I.. -D RTMIDI_EXPORT -D $(MIDI_API)
+
+ifeq ($(findstring clang,$(CC)), clang)
+	CCFLAGS += -Wextra
+endif
+
+#
+# Toolchain
+#
+
+ifndef AR
+  AR = ar
+endif
+
+ifndef CC
+  CC = clang
+endif
+
+ifndef STRIP
+  STRIP = strip
+endif
+
+#
+# Building rules
+#
+
+all: $(TARGET)
+
+clean:
+	rm -f $(TARGET) $(OBJS)
+
+copy: $(TARGET)
+	cp $(TARGET) ../../Packages/jp.keijiro.rtmidi/Runtime/Plugins/$(PLATFORM)
+
+$(OBJ_DIR)/$(PRODUCT).dll: $(OBJS)
+	$(CC) $(LDFLAGS) -o $@ $^
+	$(STRIP) $@
+
+$(OBJ_DIR)/lib$(PRODUCT).dylib: $(OBJS)
+	$(CC) $(LDFLAGS) -o $@ $^ $(LIBS)
+
+$(OBJ_DIR)/lib$(PRODUCT).a: $(OBJS)
+	$(AR) -crv $@ $^
+
+$(OBJ_DIR)/%.o: ../Source/%.cpp | $(OBJ_DIR)
+	$(CC) $(CCFLAGS) -c -o $@ $<
+
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
