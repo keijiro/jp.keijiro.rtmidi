87f00853612ae8a3f43fb351b08887a8f3429305
Keijiro Takahashi <keijiro@gmail.com>
2025-05-02 10:24:15 +0900

    simplification

--- RtMidi/Windows/build.sh
+++ RtMidi/Windows/build.sh
@@ -4,7 +4,5 @@ STRIP="x86_64-w64-mingw32-strip"
 CCFLAGS="-O2 -Wall -I../Source -D RTMIDI_EXPORT -D __WINDOWS_MM__"
 LDFLAGS="-shared"
 LIBS="-Wl,-Bstatic -lwinmm -static-libgcc"
-$CC $CCFLAGS -c ../Source/RtMidi.cpp
-$CC $CCFLAGS -c ../Source/rtmidi_c.cpp
-$CC $LDFLAGS RtMidi.o rtmidi_c.o $LIBS -o RtMidi.dll
+$CC $CCFLAGS $LDFLAGS ../Source/RtMidi.cpp ../Source/rtmidi_c.cpp $LIBS -o RtMidi.dll
 $STRIP RtMidi.dll
